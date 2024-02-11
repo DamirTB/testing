6588ed3328fef4392a33ae1c3da970682de29ba6
DamirTB <talipdamir@gmail.com>
2024-02-11 21:21:51 +0600

    Updated app.js & app.css to display posts from back-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [listofPosts, setListOfPosts] = useState([]);
   useEffect(() =>{
-    axios.get("")
+    axios.get("http://localhost:3001/posts").then((response) => {
+    setListOfPosts(response.data);  
+    //console.log(response);
+    });
   }, [])
-  return (<div className="App"></div>);
+  return (<div className="App">
+    {listofPosts.map((value, key) => {return <div className="post"> 
+      <div className="title"> {value.title}, </div>
+      <div className="body"> {value.postText} </div>
+      <div className="footer"> {value.username} </div>
+    </div>})}
+  </div>);
 }
 
 export default App;
